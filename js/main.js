00fa68bcdcee57fcdbdf90b3d98ee7ba7bb9b330
Taras Danylyuk <tarik.danylyuk@gmail.com>
2017-02-21 18:51:41 +0200

    added 45deg

--- js/main.js
+++ js/main.js
@@ -3,89 +3,111 @@ document.addEventListener("DOMContentLoaded", ready);
 function ready() {
     "use strict";
 
-    let current = getBallPostion();
-
-    let computed = computeBallPosition(175, current);
-    console.log(computed);
-
-    setBallSpeedAndPosition(computed.speed, computed.target);
-
-
-
-
-    setTimeout(() => {
-        console.log(computeBallPosition(90 - 5, computed.target).target);
-
-    }, computed.speed * 1000);
-}
-
-
-
-function getBallPostion() {
-    "use strict";
-
-    let DOMBall = document.querySelector('.ball');
-
-    let top = window.getComputedStyle(DOMBall).getPropertyValue('top');
-    let left = window.getComputedStyle(DOMBall).getPropertyValue('left');
-
-    return {
-        top: +top.substring(0, top.length - 2),
-        left: +left.substring(0, left.length - 2)
-    }
-}
-
-function computeBallPosition(deg, current) {
-    "use strict";
     const BALL_SPEED = 400;
+    const RAD = Math.PI / 180;
+    const DOMBall = document.querySelector('.ball');
 
-    let target = {
-        top: 0,
-        left: 0
-    };
-    let goBottom = false;
 
-    if(deg > 90) {
-        deg = 180 - deg;
-        goBottom = true;
+    let current = getBallPosition();
+
+    start(20, 2, current);
+
+
+
+
+    function start(deg, sector, current) {
+        let target = computeBallPosition(deg, sector, current);
+        setBallSpeedAndPosition(target);
+
+        setTimeout(() => {
+            start(deg, target.sector, target);
+        }, target.speed * 1000);
     }
 
-    let left = Math.tan(deg * Math.PI / 180) * current.top;
 
-    console.log(left, 'left');
+    function computeBallPosition(deg, sector, current) {
+        let target = {
+            top: 0,
+            left: 0
+        };
 
-    if (left <= 750) {
-        target.left = left;
+        let a = Math.tan(deg * RAD);
 
-        if (goBottom) {
-            target.top = 750;
+        switch(sector) {
+            case 1:
+                if(current.top == 800) {
+                    target.left = 0;
+                    target.top = Math.round(800 - (a * current.left));
+                    target.sector = 2;
+                }
+                else {
+                    target.left = Math.round(800 - (a * current.top));
+                    target.top = 0;
+                    target.sector = 4;
+                }
+                break;
+            case 2:
+                if(current.top == 800) {
+                    target.left = 800;
+                    target.top = Math.round(800 - (a * (800 -current.left)));
+                    target.sector = 1;
+                }
+                else {
+                    target.left = Math.round(a * current.top);
+                    target.top = 0;
+                    target.sector = 3;
+                }
+                break;
+            case 3:
+                if(current.top == 0) {
+                    target.left = 800;
+                    target.top = Math.round(a * (800 - current.left));
+                    target.sector = 4;
+                }
+                else {
+                    target.left = Math.round(a * (800 - current.top));
+                    target.top = 800;
+                    target.sector = 2;
+                }
+                break;
+            case 4:
+                if(current.top == 0) {
+                    target.left = 0;
+                    target.top = Math.round(a * (current.left));
+                    target.sector = 3;
+                }
+                else {
+                    target.left = Math.round(800 - (a * (800 - current.top)));
+                    target.top = 800;
+                    target.sector = 1;
+                }
+                break;
+            default:
+                break;
         }
-        else {
-            target.top = 0;
+
+        let distance = Math.pow(Math.pow(current.top - target.top, 2) + Math.pow(current.left - target.left, 2), 0.5);
+        target.speed = distance / BALL_SPEED;
+
+        return target;
+    }
+
+    function getBallPosition() {
+        let top = window.getComputedStyle(DOMBall).getPropertyValue('top');
+        let left = window.getComputedStyle(DOMBall).getPropertyValue('left');
+
+        return {
+            top: +top.substring(0, top.length - 2),
+            left: +left.substring(0, left.length - 2)
         }
     }
-    else {
-        target.left = 750;
-        target.top = Math.tan((90 - deg) * Math.PI / 180) * (left - 800);
 
-        if(goBottom) target.top = 800 - target.top;
-    }
-
-
-    let distance = Math.pow(Math.pow(current.top - target.top, 2) + Math.pow(current.left - target.left, 2), 0.5);
-    let speed = distance / BALL_SPEED;
-
-    return {
-        target,
-        speed
+    function setBallSpeedAndPosition(target) {
+        DOMBall.style.transition = target.speed + 's linear';
+        DOMBall.style.top = target.top + 'px';
+        DOMBall.style.left = target.left + 'px';
     }
 }
 
-function setBallSpeedAndPosition(speed, target) {
-    "use strict";
 
-    let DOMBall = document.querySelector('.ball');
-    DOMBall.style.transition = speed + 's linear';
-    DOMBall.style.top = target.top + 'px';
-    DOMBall.style.left = target.left + 'px';
-}
+
